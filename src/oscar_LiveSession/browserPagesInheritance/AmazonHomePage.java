daa53d293bd5e6db939dc237e785eee6f23d828a
ulkucardak <ulkucardak@gmail.com>
2022-04-22 12:17:10 +0100

    New Oscars example added

---
+++ src/oscar_LiveSession/browserPagesInheritance/AmazonHomePage.java
@@ -0,0 +1,27 @@
+package oscar_LiveSession.browserPagesInheritance;
+
+public class AmazonHomePage extends Browser{
+
+    public String product;
+
+    public AmazonHomePage(String name) { // super class constructor always executes before subclass constructor
+        super(name);
+    }
+
+    public void navigateTo(String link){
+        // normally we will have selenium code but for right now we will just use println to understand inheritance
+        System.out.println("Navigating to "+link+" using "+super.getName());
+    }
+
+    void shopFor(String product){
+        this.product=product;  // instead of initializing in the constructor I can do it in this method as well
+        System.out.println("Navigating to "+this.product+" 's Page");
+    }
+
+    @Override
+    public String toString() {
+        return "AmazonHomePage{" +
+                "product='" + product + '\'' +
+                '}';
+    }
+}
---
+++ src/oscar_LiveSession/browserPagesInheritance/CartPage.java
@@ -0,0 +1,20 @@
+package oscar_LiveSession.browserPagesInheritance;
+
+public class CartPage extends AmazonHomePage{
+
+    public String buyerInfo;
+    public CartPage(String name, String buyerInfo) {
+        super(name);
+        this.buyerInfo=buyerInfo;
+    }
+
+    public void fillInfo(){
+        System.out.println("Filling the necessary fields with :"+buyerInfo);
+    }
+
+    public void payFor(long cardNumber){
+        System.out.println("Paying with credit card : "+cardNumber);
+    }
+
+
+}

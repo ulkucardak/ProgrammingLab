119aa2bebddf230d6a7e8855aff9e52fa2bfc51a
ulkucardak <ulkucardak@gmail.com>
2022-04-29 01:20:01 +0100

    Custom Exceptions examples added

---
+++ src/oscar_LiveSession/exceptions/customExceptios/BreakTimeException.java
@@ -0,0 +1,8 @@
+package oscar_LiveSession.exceptions.customExceptios;
+
+
+public class BreakTimeException extends RuntimeException{
+    public BreakTimeException(String message){
+        super(message); // call the constructor of RunTimeException class
+    }
+}
---
+++ src/oscar_LiveSession/exceptions/customExceptios/InsufficientBalanceException.java
@@ -0,0 +1,9 @@
+package oscar_LiveSession.exceptions.customExceptios;
+
+public class InsufficientBalanceException extends RuntimeException{
+
+    public InsufficientBalanceException(){}
+
+    public InsufficientBalanceException(String message){super(message);}
+
+}
